d44be770c9ed4193193dabcfa96e2bcfc8a740cb
Azeem Shaikh <azeem6755@gmail.com>
2024-12-31 19:26:48 +0530

    Add CRUD to articles

---
+++ backend/app/data.py
@@ -0,0 +1,14 @@
+from models import Article
+
+article_list = {
+    0: Article(id=0, title='Some Title 1', link='localhost:8080'),
+    1: Article(id=1, title='Some Title 2', link='localhost:8080'),
+    2: Article(id=2, title='Some Title 3', link='localhost:8080'),
+    3: Article(id=3, title='Some Title 4', link='localhost:8080'),
+    4: Article(id=4, title='Some Title 5', link='localhost:8080'),
+    5: Article(id=5, title='Some Title 6', link='localhost:8080'),
+    6: Article(id=6, title='Some Title 7', link='localhost:8080'),
+    7: Article(id=7, title='Some Title 8', link='localhost:8080'),
+    8: Article(id=8, title='Some Title 9', link='localhost:8080'),
+    9: Article(id=9, title='Some Title 10', link='localhost:8080'),
+}
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,53 @@
 from fastapi import FastAPI
-
+from data import article_list
+from fastapi.exceptions import HTTPException
 
 app = FastAPI()
 
+fake_ass_data = [{'name': 'Azeem', 'mobile': 123}, {'name': 'Mariya', 'mobile': 345}, {'name': 'Zuni', 'mobile': 100}]
+
+@app.get('/items/')
+async def get_items(skip: int = 0, limit: int = 10):
+    return fake_ass_data[skip: skip + limit]
+
 @app.get('/')
 async def root():
-    return {'message': 'Hello World!!!'}
+    return {'message': 'Welcome to Content Aggregator'}
+
+@app.get('/article/')
+async def get_all_articles():
+    return article_list
+
+
+@app.get('/article/{article_id}')
+async def get_single_article(article_id: int):
+    if article_id not in article_list:
+        raise HTTPException(status_code=404, detail=f'Article with {article_id=} does not exist.')
+    article = article_list.get(article_id)
+    return article
+
+@app.delete('/delete-article/{article_id}')
+async def delete_article(article_id: int):
+    if article_id not in article_list:
+        raise HTTPException(status_code=404, detail=f'Article with {article_id=} does not exist.')
+    article = article_list.pop(article_id)
+    return article
+
+
+@app.put('/update-article/{article_id}')
+async def update_article(article_id: int, id: int | None = None, title: str|None = None, link: str|None = None):
+    if article_id not in article_list:
+        raise HTTPException(status_code=404, detail=f'Article with {article_id=} does not exist.')
+    article = article_list[article_id]
+    print(title)
+    if id is not None:
+        article.id = id
+
+    if title is not None:
+        article.title = title
+
+    if link is not None:
+        article.link = link
+
+    return {'updated': article}
+        
